51e4dd204af889c547b5bc3686ee39c483a852ff
obedAlarcon <desarrolloc20@hotmail.com>
2025-11-25 12:21:43 -0500

    correo emailjs

--- src/app/components/portafolio/contact/contact.component.ts
+++ src/app/components/portafolio/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import * as emailjs from '@emailjs/browser';
 
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -14,10 +15,14 @@ import { ContactService } from '../../../services/contact.service';
 })
 export default class ContactComponent {
   contactForm!: FormGroup;
-
   loading = false;
 
-  constructor(private fb: FormBuilder, private contactService:ContactService) {}
+  // Configura tus datos de EmailJS
+  private serviceID = 'TU_SERVICE_ID';
+  private templateID = 'TU_TEMPLATE_ID';
+  private publicKey = 'TU_PUBLIC_KEY';
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.contactForm = this.fb.group({
@@ -34,38 +39,37 @@ export default class ContactComponent {
     }
 
     this.loading = true;
-  this.contactService.sendMessage(
-  this.contactForm.value.name,
-  this.contactForm.value.email,
-  this.contactForm.value.message
-).subscribe({
-  next: () => {
-    Swal.fire({
-        icon: 'success',
-        title: 'Mensaje enviado ',
-        text: 'Tu mensaje fue enviado correctamente.',
-        confirmButtonColor: '#10b981', // verde
-        background: '#1f2937',         // gris oscuro
-        color: 'white'
-      });
-      this.contactForm.reset();
-      this.loading = false;
-    
-  },
-  error: (err: any) => {
-    console.error(err);
-    Swal.fire({
-      icon: 'error',
-      title: 'Error al enviar',
-      text: 'No se pudo enviar el mensaje. Intenta de nuevo.',
-      confirmButtonColor: '#ef4444',
-      background: '#1f2937',
-      color: 'white'
-    });
-    this.loading = false;
-  }
-});
 
-    
+    const templateParams = {
+      name: this.contactForm.value.name,
+      email: this.contactForm.value.email,
+      message: this.contactForm.value.message
+    };
+
+    emailjs.send(this.serviceID, this.templateID, templateParams, this.publicKey)
+      .then(() => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Mensaje enviado',
+          text: 'Tu mensaje fue enviado correctamente.',
+          confirmButtonColor: '#10b981', // verde
+          background: '#1f2937',         // gris oscuro
+          color: 'white'
+        });
+        this.contactForm.reset();
+        this.loading = false;
+      })
+      .catch((err: any) => {
+        console.error(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error al enviar',
+          text: 'No se pudo enviar el mensaje. Intenta de nuevo.',
+          confirmButtonColor: '#ef4444',
+          background: '#1f2937',
+          color: 'white'
+        });
+        this.loading = false;
+      });
   }
 }
